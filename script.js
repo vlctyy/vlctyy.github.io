343775e6af0dd1e6810a09e5806ad26a753082ec
vlctyy <kharispowell@hotmail.com>
2025-02-26 12:07:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-function openTab(evt, tabName) {
-    let tabContent = document.getElementsByClassName("tab-content");
-    for (let i = 0; i < tabContent.length; i++) {
-        tabContent[i].classList.remove("active");
-    }
-    document.getElementById(tabName).classList.add("active");
-}
-
 function copyToClipboard() {
-    let codeSnippet = document.getElementById("code-snippet").innerText;
+    const codeSnippet = document.getElementById("code-snippet").innerText;
     navigator.clipboard.writeText(codeSnippet).then(() => {
-        alert("Copied to clipboard!");
-    }).catch(err => {
-        console.error("Failed to copy: ", err);
+        alert("Copied!");
     });
 }
+
+function showTab(tabName) {
+    document.querySelectorAll(".tab-content").forEach(tab => {
+        tab.style.display = "none";
+    });
+    document.getElementById(tabName).style.display = "block";
+
+    document.querySelectorAll(".tab-button").forEach(button => {
+        button.classList.remove("active");
+    });
+    event.target.classList.add("active");
+}
+
+/* Show first tab by default */
+document.getElementById("bloxstrap").style.display = "block";
